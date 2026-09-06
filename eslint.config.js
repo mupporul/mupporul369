10da0af1982ece9a2df34c5995291477ef76b51e
sivgan <sivganeshmukundan@gmail.com>
2026-09-06 19:54:45 +0100

    feat: migrate user storage to Supabase

--- eslint.config.js
+++ eslint.config.js
@@ -18,4 +18,13 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+  {
+    files: ['backend/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
 ])
